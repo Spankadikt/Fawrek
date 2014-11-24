6d1bc7f53b3a975b9f4e349287785f7a6c531f8d
spankadikt <maxime.schwob@gmail.com>
2014-11-24 23:13:24 +0100

    First tests to test c++ dll and c# wrapper. Testing procedural mesh, bones and animation generation.

---
+++ PINKRIVER/PINKCREEK/TestCSharpLibrary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace TestCSharpLibrary
+{
+    public class TestCSharpLibrary
+    {
+        // From c++ Dll (unmanaged)
+        [DllImport("PinkSpring")]
+        public static extern float TestMultiply(float a, float b);
+
+        // From c++ Dll (unmanaged)
+        [DllImport("PinkSpring")]
+        public static extern float TestDivide(float a, float b);
+
+
+        public static float SharpMultiply(float a, float b)
+        {
+            return (a * b);
+        }
+
+        public static float SharpDivide(float a, float b)
+        {
+            if (b == 0)
+            {
+                return 0;
+            }
+
+            return (a / b);
+        }
+    }
+}
